0ab529ff211eec9e4fd215828115c1747f1ee142
mihir1811 <mihir.roxontechnologies@gmail.com>
2023-03-18 01:08:17 +0530

    change fonts

--- components/posts/Posts.js
+++ components/posts/Posts.js
@@ -6,14 +6,20 @@ const Posts = () => {
     const [posts , setPosta] = useState(postData)
 
   return (
-    <div style={{height:"100%"}} className="max-w-[700px] mx-auto scrollbar-hide px-2">
-        {
-            posts.length > 0 && posts.map((post ,i)=>{
-                return <div key={i} className="bg-slate-200 rounded-[10px] my-2">
-                    <SinglePost post={post}  />
-                </div>
-            })
-        }
+    <div className='h-full flex'>
+        <div style={{height:"100%"}} className="max-w-[700px] sm:w-[100%] mx-auto scrollbar-hide px-2">
+            {
+                posts.length > 0 &&  posts.map((post ,i)=>{
+                    return <div key={i} className="bg-slate-200 rounded-[10px] my-2">
+                        <SinglePost post={post}  />
+                    </div>
+                })
+            }
+        </div>
+        <div className='w-[30%] bg-[gray] h-screen lg:block hidden'>
+            <h3>Chat</h3>
+
+        </div>
     </div>
   )
 }
